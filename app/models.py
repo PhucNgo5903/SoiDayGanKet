769dd9cf536bf78465914fcc24e8d3fbc5ae6f7c
phucngo <phucngo@gmail.com>
2025-03-16 21:33:42 +0700

    Login, Registration function on process, 3 main homepage created (access without login)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Donor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=15, null = True)
+    userpic = models.ImageField(upload_to='donor', null= True, blank=True)
+    regdate = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.user.username
+
+class Volunteer (models. Model):
+
+    user = models. ForeignKey (User, on_delete=models.CASCADE)
+    contact = models. CharField(max_length=15, null=True)
+    userpic = models. ImageField(upload_to='volunteer', null=True, blank=True)
+    regdate = models. DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
